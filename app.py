04011eb84b4324adcec804d4c14258eddfedf513
Sohyun Sim <sohyun.sim27@gmail.com>
2023-07-19 18:51:15 +0900

    ui update

--- app.py
+++ app.py
@@ -6,18 +6,64 @@ import requests
 import openai
 import gradio as gr
 
-from src.process.shopping import ShoppingDataLoader
+from src.process.shopping import ShoppingPromptTool
 
-preset_title = {0: "네이버 쇼핑 트렌드"}
-preset_args = {0:{
-                "child_id":"50001420",
-                "mid_id":"50000212", 
-                "root_id":"50000003", 
-                "type_id":"purchase"
-                }}
+import gradio as gr
 
-def create_content(user_content, system_content, openai_key):
+theme = gr.themes.Default(
+    primary_hue="blue",
+).set(
+    block_shadow='none',
+    block_shadow_dark='none',
+    form_gap_width='0px',
+    checkbox_label_background_fill='*button_secondary_background_fill',
+    checkbox_label_background_fill_dark='*button_secondary_background_fill',
+    checkbox_label_background_fill_hover='*button_secondary_background_fill_hover',
+    checkbox_label_background_fill_hover_dark='*button_secondary_background_fill_hover',
+    checkbox_label_shadow='none',
+    error_background_fill_dark='*background_fill_primary',
+    input_background_fill='*neutral_100',
+    input_background_fill_dark='*neutral_700',
+    input_border_width='0px',
+    input_border_width_dark='0px',
+    input_shadow='none',
+    input_shadow_dark='none',
+    input_shadow_focus='*input_shadow',
+    input_shadow_focus_dark='*input_shadow',
+    stat_background_fill='*primary_300',
+    stat_background_fill_dark='*primary_500',
+    button_shadow='none',
+    button_shadow_active='none',
+    button_shadow_hover='none',
+    button_transition='background-color 0.2s ease',
+    button_primary_background_fill='*primary_200',
+    button_primary_background_fill_dark='*primary_700',
+    button_primary_background_fill_hover='*button_primary_background_fill',
+    button_primary_background_fill_hover_dark='*button_primary_background_fill',
+    button_primary_border_color_dark='*primary_600',
+    button_secondary_background_fill='*neutral_200',
+    button_secondary_background_fill_dark='*neutral_600',
+    button_secondary_background_fill_hover='*button_secondary_background_fill',
+    button_secondary_background_fill_hover_dark='*button_secondary_background_fill',
+    button_cancel_background_fill='*button_secondary_background_fill',
+    button_cancel_background_fill_dark='*button_secondary_background_fill',
+    button_cancel_background_fill_hover='*button_cancel_background_fill',
+    button_cancel_background_fill_hover_dark='*button_cancel_background_fill',
+    button_cancel_border_color='*button_secondary_border_color',
+    button_cancel_border_color_dark='*button_secondary_border_color',
+    button_cancel_text_color='*button_secondary_text_color',
+    button_cancel_text_color_dark='*button_secondary_text_color'
+)
+
+title_preset = {0: "네이버 쇼핑 트렌드"}
+item_preset  = {0: {"상품" : ["선풍기", "블랙박스", "냉장고"],
+                  "순위 유형": ["많이 본 상품", "많이 구매한 상품", "인기 브랜드", "트렌드 키워드"]}}
+script_preset = {0: "간단히 요약하기",
+                 1: "호기심을 유발하기"}
+
+def create_content(tool, openai_key):
     openai.api_key = openai_key
+    system_content, user_content = tool.get_fullprompt()
     messages = [{"role": "system", "content": system_content},
                 {"role": "user", "content": user_content}]
     chat = openai.ChatCompletion.create(
@@ -26,27 +72,54 @@ def create_content(user_content, system_content, openai_key):
     )
     return chat.choices[0].message.content
 
-def get_preset_template(preset_id):
+def get_tool(preset_id):
     if preset_id == 0:
-        preset_loader = ShoppingDataLoader(**preset_args[preset_id])
+        tool = ShoppingPromptTool()
+    return tool
 
-    return preset_loader.get_prompt()
+def get_itemlist(preset_id):
+    params = list(item_preset[preset_id].items())
+    is_interactive = [True if len(_) > 0 else False for _ in item_preset[preset_id].values()]
+    return param1.update(choices=params[0][1], label=params[0][0], interactive=is_interactive[0]), \
+        param2.update(choices=params[1][1], label=params[1][0], interactive=is_interactive[1])
 
+def load_data(dataloader, *args):    
+    args = (_ for _ in args if len(_) > 0)
+    data, prompt = dataloader(*args)
+    return data, prompt
 
-with gr.Blocks() as demo:
+def get_image_file(tool):
+    tool.save_image()
+
+with gr.Blocks(theme=theme) as demo:
     with gr.Row():
-        api_key = gr.Textbox(placeholder="sk-...", label="OPENAI_API_KEY", value=None, type="password")
+        with gr.Column(variant="panel"):
+            gr.Markdown("## 1 컨텐츠 내용 선정하기")
+            content_titles = gr.Dropdown(list(title_preset.values()), info="주제를 선택하세요.", type='index', label="주제")
+            with gr.Row(variant="compact") as item_params:
+                param1 = gr.Dropdown([], visible=True, interactive=False, label="세부 주제1")
+                param2 = gr.Dropdown([], visible=True, interactive=False, label="세부 주제2")
+            prompt_type = gr.Dropdown(list(script_preset.values()), info="어떤 컨텐츠를 만들고 싶은지 선택하세요.", type='index', label="스크립트 스타일")
+            tool = gr.State()
+            with gr.Accordion("데이터 직접 업로드하기", open=False):
+                data_file = gr.File(label="Local file")
+            btn_data = gr.Button("데이터와 프롬프트 가져오기", variant="secondary")
+        with gr.Column():
+            gr.Markdown("## 2 스크립트 만들기")
+            data = gr.Text(label="컨텐츠 관련 자료", max_lines=5)
+            prompt = gr.Textbox(interactive=True, max_lines=1, label="프롬프트")
+            api_key = gr.Textbox(placeholder="sk-...", label="OPENAI_API_KEY", value=None, type="password")
+            output = gr.Textbox(interactive=False, max_lines=5, label="스크립트", show_copy_button=True)
+    generate = gr.Button("스크립트 생성하기", variant="primary")
     with gr.Row():
-        with gr.Column():
-            preset = gr.Dropdown(list(preset_title.values()), type='index', label="Preset")
-            # input_data = gr.File(label="제작에 참고할 데이터: txt, csv, json")
-            data = gr.Text(label="제작에 참고할 데이터")
-        with gr.Column():
-            prompt = gr.Textbox(placeholder="컨텐츠 유형과 제작 방향성, 분량 등을 적어주세요.", lines=5, label="제작 요청 사항")
-            output = gr.Textbox(interactive=False, lines=5, label="결과물")
-    generate = gr.Button("생성하기")
+        save_image = gr.Button("컨텐츠 이미지 저장하기")
+        save_data = gr.Button("컨텐츠 이미지 저장하기")
+        save_prompt = gr.Button("프롬프트 저장하기")
 
-    preset.change(fn=get_preset_template, inputs=preset, outputs=[data, prompt])
-    generate.click(fn=create_content, inputs=[prompt, data, api_key], outputs=output)
+    content_titles.change(fn=get_tool, inputs=[content_titles], outputs=[tool]).then(get_itemlist, [content_titles], [param1, param2])
+    btn_data.click(fn=load_data, inputs=[tool, param1, param2], outputs=[data, prompt])
+    generate.click(fn=create_content, inputs=[tool, api_key], outputs=output)
+    save_image.click(fn=get_image_file, inputs=[tool])
     
-demo.launch()
+if __name__ == "__main__":
+    demo.queue(concurrency_count=10).launch()
--- src/process/shopping.py
+++ src/process/shopping.py
@@ -1,3 +1,4 @@
+import os
 import bs4
 import urllib
 import json
@@ -7,6 +8,11 @@ from src.utils import (
     save_img,
     get_today
 )
+
+with open("data/shopping_id.json", "r", encoding="utf-8") as f:
+    categoryDetail = json.load(f)
+id2midCategory = categoryDetail["id2midCategory"]
+id2childCategory = categoryDetail["childCategories"]
 
 id2rootCategory = {'50000000': '패션의류',
                     '50000001': '패션잡화',
@@ -19,81 +25,41 @@ id2rootCategory = {'50000000': '패션의류',
                     '50000008': '생활/건강',
                     '50000009': '여가/생활편의',
                     'ALL': '전체'}
-id2midCategory = {'50000087': '학습기기',
-                '50000088': '게임기/타이틀',
-                '50000089': 'PC',
-                '50000090': 'PC액세서리',
-                '50000091': '노트북액세서리',
-                '50000092': '태블릿PC액세서리',
-                '50000093': '모니터주변기기',
-                '50000094': '주변기기',
-                '50000095': '멀티미디어장비',
-                '50000096': '저장장치',
-                '50000097': 'PC부품',
-                '50000098': '네트워크장비',
-                '50000099': '소프트웨어',
-                '50000151': '노트북',
-                '50000152': '태블릿PC',
-                '50000153': '모니터',
-                '50000204': '휴대폰',
-                '50000205': '휴대폰액세서리',
-                '50000206': '카메라/캠코더용품',
-                '50000208': '영상가전',
-                '50000209': '음향가전',
-                '50000210': '생활가전',
-                '50000211': '이미용가전',
-                '50000212': '계절가전',
-                '50000213': '주방가전',
-                '50000214': '자동차기기'}
-id2childCategory = {'50001419': '냉풍기',
-                '50001420': '선풍기',
-                '50001421': '에어컨',
-                '50001422': '온풍기',
-                '50001423': '온수기',
-                '50001424': '보일러',
-                '50001425': '가습기',
-                '50001426': '공기정화기',
-                '50001427': '제습기',
-                '50001428': '전기매트/장판',
-                '50001429': '전기요/담요/방석',
-                '50001851': '냉온풍기',
-                '50006834': '에어컨주변기기',
-                '50006971': '업소용냉온풍기',
-                '50009120': '온수매트',
-                '50009180': '히터'}
+
+preset_ids = {"선풍기": {"child_id": "50001420",
+                       "mid_id": "50000212",
+                       "root_id": "50000003"},
+              "냉장고": {"child_id": "50001430",
+                       "mid_id": "50000213",
+                       "root_id": "50000003"},
+              "블랙박스": {"child_id": "50001188",
+                        "mid_id": "50000214",
+                        "root_id": "50000003"},
+              "ALL": {"root_id":"ALL"}}
+
 id2type = {"click": "많이 본 상품", "purchase": "많이 구매한 상품", "brand": "인기 브랜드", "keyword": "트렌드 키워드"}
+type2id = {v:k for k, v in id2type.items()}
 
+class ShoppingPromptTool():
+    def __init__(self):
+        print("ShoppingDataLoader")
 
-child_id = "50001420"
-mid_id = "50000212"
-root_id = "50000003"
-type_id = "purchase"
+    def setup(self, child_category, trend_type):
+        self.child_name = child_category
+        self.trend_type = trend_type
 
-class ShoppingDataLoader():
-    def __init__(self, child_id, mid_id, root_id, type_id):
-        request_url = f"https://search.shopping.naver.com/best/category/{type_id}?categoryCategoryId={child_id}&categoryChildCategoryId={child_id}&categoryDemo=A00&categoryMidCategoryId={mid_id}&categoryRootCategoryId={root_id}&period=P1D"
-        self.data = self._load_data(request_url)
-        self.child_id = child_id
-        self.type_id = type_id
-        self.child_name = id2childCategory[child_id]
-        self.trend_type = id2type[type_id]
-        self.date = get_today()
+    def __call__(self, child_category, trend_type):
+        self.setup(child_category, trend_type)
+        try:
+            param_ids = preset_ids[child_category]
+        except:
+            param_ids = preset_ids["ALL"]
+        param_ids["type_id"] = type2id[trend_type]
+        return self.get_data(**param_ids), self.get_prompt()
 
-
-    def _load_data(self, url):
-        source = urllib.request.urlopen(url).read()
-        soup = bs4.BeautifulSoup(source, "lxml")
-        trend_json = json.loads(soup.select("script#__NEXT_DATA__")[0].text)
-        trend_queries = trend_json['props']['pageProps']['dehydratedState']['queries']
-        for item in trend_queries:
-            if item['queryKey'][0] == 'CATEGORY_PRODUCTS':
-                trend_df = pd.DataFrame(item['state']['data']['products'][:10], columns=["rank", "imageUrl", "mobileLowPrice", "productTitle"])
-                break
-        return trend_df
-
-    def save_image(self, save_dir, drop=True):
-        for i, row in self.data.iterrows():
-            save_img(row['imageUrl'], [self.date, self.child_id, row["rank"]], save_dir)
+    def save_image(self, save_dir="/Downloads", drop=True):
+        for _, row in self.data.iterrows():
+            save_img(row['imageUrl'], [get_today(), self.type_id, self.child_id, row["rank"]], save_dir)
         if drop:
             self.data = self.data.drop(columns=["imageUrl"])
     
@@ -103,7 +69,44 @@ class ShoppingDataLoader():
         else:
             return self.data.to_markdown()
     
-    def get_prompt(self, **kwargs):
-        system_content = "제목 : {} {} {} 베스트 10 \n{}".format(self.date, self.child_name, self.trend_type, self.to_markdown())
-        user_content = "오늘 {}의 {}을 요약해서 소개해줘".format(self.child_name, self.trend_type)
-        return system_content, user_content
+    def get_data(self, type_id="purchase", child_id="", mid_id="", root_id="ALL", output_type="markdown"):
+
+        def _load_json(request_url, query, cols, data_key):
+            source = urllib.request.urlopen(request_url).read()
+            soup = bs4.BeautifulSoup(source, "lxml")
+            trend_json = json.loads(soup.select("script#__NEXT_DATA__")[0].text)
+            trend_queries = trend_json['props']['pageProps']['dehydratedState']['queries']
+            for item in trend_queries:
+                if item['queryKey'][0] == query:
+                    df = pd.DataFrame(item['state']['data'][data_key][:10], columns=cols)
+                    return df
+
+        if type_id in ["click", "purchase"]:
+            request_url = f"https://search.shopping.naver.com/best/category/{type_id}?categoryCategoryId={child_id}&categoryChildCategoryId={child_id}&categoryDemo=A00&categoryMidCategoryId={mid_id}&categoryRootCategoryId={root_id}&period=P1D"
+            query = "CATEGORY_PRODUCTS"
+            cols = ["rank", "imageUrl", "mobileLowPrice", "productTitle"]
+            data_key = 'products'
+        elif type_id in ["brand", "keyword"]:
+            request_url = f"https://search.shopping.naver.com/best/category/{type_id}?categoryCategoryId={mid_id}&categoryChildCategoryId=&categoryDemo=A00&categoryMidCategoryId={mid_id}&categoryRootCategoryId={root_id}&chartRank=1&period=P1D"
+            query = "CHART_LIST"
+            data_key = 'charts'
+            if type_id == "brand":
+                cols = ["rank", "change", "exposeBrandName"]
+            elif type_id == "keyword":
+                cols = ["rank", "change", "exposeKeyword"]
+
+        self.data = _load_json(request_url, query, cols, data_key)
+
+        if output_type == "markdown":
+            return self.to_markdown()
+        else:
+            return self.data.drop(columns=["imageUrl"])
+
+    def get_fullprompt(self):
+        system_content = "제목 : {} {} {} 베스트 10 \n{}".format(get_today(), self.child_name, self.trend_type, self.to_markdown())
+        user_content = "오늘 {} {} 요약해서 소개해줘".format(self.child_name, self.trend_type)
+        return (system_content, user_content)
+
+    def get_prompt(self):
+        user_content = "오늘 {} {} 요약해서 소개해줘".format(self.child_name, self.trend_type)
+        return user_content
